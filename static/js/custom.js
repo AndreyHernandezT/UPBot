25a8216404f96fda01c31870ef188b0e95d527a9
Andrey Hernandez Torres <andreyhernandeztorres@gmail.com>
2021-05-12 16:27:13 -0500

    Diseño Completo

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,34 @@
-$(document).ready(function() {
-	
-
-	// $('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'})
+$(document).ready(function () {
+	$('.chat_icon').click(function () {
+		$('.chat_box').toggleClass('active');
+	});
+	$("form").on("submit", function (event) {
+		var rawText = $("#text").val();
+		if ($.trim(rawText) == '') {
+			return false;
+		}
+		var userHtml = '<li class="resUser"><p class="userText">' + rawText + "</p></li>";
+		$("#text").val("");
+		$(".messages").append(userHtml);
+		document.getElementById("userInput").scrollIntoView({
+			block: "start",
+			behavior: "smooth",
+		});
+		$.ajax({
+			data: {
+				msg: rawText,
+			},
+			type: "POST",
+			url: "/get",
+		}).done(function (data) {
+			var botHtml = '<li class="resChat"><p class="botText">' + data + "</p></li>";
+			$(".messages").append($.parseHTML(botHtml));
+			document.getElementById("userInput").scrollIntoView({
+				block: "start",
+				behavior: "smooth",
+			});
+		});
+		$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+		event.preventDefault();
+	});
 });
