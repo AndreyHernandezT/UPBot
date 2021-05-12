4831159c0715d20d4e9f4dba9ea6354c0debdcc6
Andrey Hernandez Torres <andreyhernandeztorres@gmail.com>
2021-05-12 03:20:17 -0500

    database ok

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-	$('.chat_icon').click(function() {
-		$('.chat_box').toggleClass('active');
-	});
+	
 
-	$('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'})
+	// $('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'})
 });
